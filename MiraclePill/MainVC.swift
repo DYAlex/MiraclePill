6b52ae61f0a0c47b51244f6666900152becdc4e1
YD <dyalex@mbpr.local>
2017-04-08 05:18:07 +0300

    MaraclePill exersice solved with StackView (including segues for SuccessView and return to MainView dismissing views)

---
+++ MiraclePill/MainVC.swift
@@ -0,0 +1,76 @@
+//
+//  ViewController.swift
+//  MiraclePill
+//
+//  Created by Mark Price on 7/10/16.
+//  Copyright © 2016 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+
+    @IBOutlet weak var statePicker: UIPickerView!
+    @IBOutlet weak var statePickerBtn: UIButton!
+    
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var countryTextField: UITextField!
+    
+    @IBOutlet weak var zipCodeLabel: UILabel!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    
+    @IBOutlet weak var buyNowBtn: UIButton!
+    
+    
+    let states = ["Alaska","Arkansas", "Alabama","California","Maine","New York"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        statePicker.dataSource = self
+        statePicker.delegate = self
+        
+       // self.view.backgroundColor = UIColor.purple
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func stateBtnPressed(_ sender: AnyObject) {
+        statePicker.isHidden = false
+        countryLabel.isHidden = true
+        countryTextField.isHidden = true
+        zipCodeLabel.isHidden = true
+        zipCodeTextField.isHidden = true
+        buyNowBtn.isHidden = true
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return states.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return states[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        statePickerBtn.setTitle(states[row], for: UIControlState()) //UIControlState.normal
+        statePicker.isHidden = true
+        countryLabel.isHidden = false
+        countryTextField.isHidden = false
+        zipCodeLabel.isHidden = false
+        zipCodeTextField.isHidden = false
+        buyNowBtn.isHidden = false
+    }
+
+
+
+}
+
---
+++ MiraclePill/SuccessVC.swift
@@ -0,0 +1,35 @@
+//
+//  SuccessVC.swift
+//  MiraclePill
+//
+//  Created by Yvette Demidova on 08/04/2017.
+//  Copyright © 2017 Devslopes. All rights reserved.
+//
+
+import UIKit
+
+class SuccessVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func successBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
